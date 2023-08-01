d91621c9ccf7214821bf5207bb0f8c106faefe83
Loulergue-jm <loulerguejm@yahoo.fr>
2023-08-01 20:03:17 +0200

    Commit du 01/08 day 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
     has_many :private_messages, through: :multi_pms
     has_many :sent_messages, class_name: "PrivateMessage",  foreign_key: "sender_id"
     has_many :received_messages, class_name: "MultiPm",  foreign_key: "recipient_id"
+    validates :first_name, presence: true
+	validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root 'gossips#index'
   
+  get '/root', to: 'gossip#index'
+  get '/welcome/:name', to: 'welcome#welcome'
   get '/team', to: 'team#team'
   get '/contact', to: 'contact#contact'
-  get '/welcome/:name', to: 'welcome#welcome'
-  root 'gossip#index'
-  get '/root', to: 'gossip#index'
-  get '/gossip/:id', to: 'gossip#show', as: 'gossip'
   get '/users/:id', to: 'users#show', as: 'user'
+  
+  resources :gossips
 end
