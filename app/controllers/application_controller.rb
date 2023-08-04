18032f02848270d6978eeba7480c6f450dc15fcc
Loulergue-jm <loulerguejm@yahoo.fr>
2023-08-04 15:45:50 +0200

    ruby 3.2.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	include SessionHelper
+	
 	def error_string(object_to_error)
 		message_array=[]
 		object_to_error.errors.full_messages.each do |msg|
