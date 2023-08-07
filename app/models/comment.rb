0cc438b958e0c1a3898d5740eae0955d20195825
Loulergue-jm <loulerguejm@yahoo.fr>
2023-08-07 09:25:42 +0200

    length comment increased to 150

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :gossip
   belongs_to :user
-  validates :content, presence: true, length: {in: 3..50}
+  validates :content, presence: true, length: {in: 3..150}
 end
